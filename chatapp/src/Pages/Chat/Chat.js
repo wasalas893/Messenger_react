a499e77f5a855e3b0f2e9acccdff563b90a6692c
wasalas <wasalas893@gmail.com>
2020-05-01 10:57:15 +0530

    change the sinup to chat page in firebase

--- chatapp/src/Pages/Chat/Chat.js
+++ chatapp/src/Pages/Chat/Chat.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import LoginString from '../Login/LoginStrings';
+
 
 
 export default class Chat extends React.Component{
     constructor(props){
         super(props)
+        this.currentUserName=localStorage.getItem(LoginString.Name);
     }
     render(){
         return(
             <div>
-                this is a chat component
+                {this.currentUserName}
             </div>
         )
     }
