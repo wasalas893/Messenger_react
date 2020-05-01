1e37a943d4e197bd609b7ab6d783d2ffc9f61b01
wasalas <wasalas893@gmail.com>
2020-05-01 10:43:45 +0530

    create the sinup to chat page in firebase

---
+++ chatapp/src/Pages/Chat/Chat.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default class Chat extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>
+                this is a chat component
+            </div>
+        )
+    }
+}
