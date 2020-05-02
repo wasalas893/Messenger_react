10604ffeb64e23498cb41d182816572f97df1496
wasalas <wasalas893@gmail.com>
2020-05-02 11:59:49 +0530

    create profile js ul

--- chatapp/src/Pages/Profile/Profile.js
+++ chatapp/src/Pages/Profile/Profile.js
@@ -20,13 +20,78 @@ export default class Profile extends React.Component{
         this.newPhoto=null
         this.newPhotoUrl=""
     }
+    //lifrciclyemethod
+    componentDidMount(){
+        if(!localStorage.getItem(LoginString.ID)){
+            this.props.history.push("/")
+        }
+    }
+    //nickename
+    onChangeNickname=(event)=>{
+      this.setState({
+          name:event.target.value
+      })
+    }
+    
     render(){
         return(
             <div className="profileroot">
             <div className="headerprofile">
                 <span>PROFILE</span>
             </div>
-            
+            <img className="avatar" alt="" src={this.state.photoUrl} />
+            <div className="viewWrapInputFile">
+                <img
+                   className="imgInputFile"
+                   alt="icon gallery"
+                   src={images.choosefile}
+                   onClick={()=>{this.refInput.click()}}
+                 />
+                <input
+                    ref={el=>{
+                        this.refInput=el
+                    }}
+                    accept="image/*"
+                    className="viewInputFile"
+                    type="file"
+                    onChange={this.onChangeAvatar}
+                />
+            </div>
+            <span className="textLabel">Name</span>
+           <input 
+               className="textInput"
+               value={this.state.name ? this.state.name:""}
+               placeholder="Your nickname..."
+               onChange={this.onChangeNickname}
+           /> 
+           <span className="textLabel">About Me</span>
+           
+           <input
+                className="textInput"
+                value={this.state.aboutMe ? this.state.aboutMe : ""}
+                placeholder="Tell aboute Yourself..."
+                onChange={this.onChangeAboutMe}
+            />
+            <div>
+                <button className="btnUpdate" onClick={this.uploadAvatar}>
+                    SAVE
+                </button>
+                <button className="btnback" onClick={()=>{this.props.history.push('/chat')}}>
+                    SAVE
+                </button>
+            </div>
+            {this.state.isLoading ?(
+                <div>
+                     <ReactLoading
+                         type={'spin'}
+                         color={'#203152'}
+                         height={'3%'}
+                         width={'3%'}
+                     />
+                </div>
+            ):null}
+
+
             </div>
         )
     }
--- chatapp/src/ProjectImages/ProjectImages.js
+++ chatapp/src/ProjectImages/ProjectImages.js
@@ -1,4 +1,6 @@
 const Images={
-   ali:require('../images/sunnada.jpg')
+   ali:require('../images/sunnada.jpg'),
+   nopic:require('../images/default_profile.jpg'),
+   choosefile:require('../images/images.png')
 }
 export default Images;
