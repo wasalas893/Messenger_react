273cbb85030dc8205c6258d7ca31e3175da14f2f
wasalas <wasalas893@gmail.com>
2020-05-03 19:47:43 +0530

    add the images

--- chatapp/src/Pages/ChatBox/ChatBox.js
+++ chatapp/src/Pages/ChatBox/ChatBox.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 
+
 export default class ChatBox extends React.Component{
     render(){
         return(
--- chatapp/src/ProjectImages/ProjectImages.js
+++ chatapp/src/ProjectImages/ProjectImages.js
@@ -1,6 +1,22 @@
 const Images={
    ali:require('../images/sunnada.jpg'),
    nopic:require('../images/default_profile.jpg'),
-   choosefile:require('../images/images.png')
+   choosefile:require('../images/images.png'),
+   input_file: require('../images/file.jpg'),
+   sticker:require('../images/sticker.jpg'),
+
+   lego1:require('../images/lego1.jpg'),
+   lego2:require('../images/lego2.jpg'),
+   lego3:require('../images/lego3.jpg'),
+   lego4:require('../images/lego4.jpg'),
+   lego5:require('../images/lego5.jpg'),
+   lego6:require('../images/lego6.png'),
+   lego7:require('../images/lego7.png'),
+   mimi1:require('../images/mimi1.png'),
+   mimi2:require('../images/mimi2.jpg'),
+   mimi3:require('../images/mimi3.png'),
+   mimi4:require('../images/mimi4.png'),
+   mimi5:require('../images/mimi5.png'),
+   
 }
 export default Images;
