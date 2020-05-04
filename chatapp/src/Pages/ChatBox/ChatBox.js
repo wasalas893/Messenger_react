fac7491cd29bb8238aa09da7804ce59c92cdcc44
wasalas <wasalas893@gmail.com>
2020-05-04 17:22:11 +0530

    create the chatbox sticker and

--- chatapp/src/Pages/ChatBox/ChatBox.js
+++ chatapp/src/Pages/ChatBox/ChatBox.js
@@ -4,9 +4,10 @@ import ReactLoading from 'react-loading';
 import 'react-toastify/dist/ReactToastify.css';
 import firebase from '../../Services/firebase';
 import images from '../../ProjectImages/ProjectImages';
-
+import moment from 'react-moment';
 import './ChatBox.css';
 import LoginString from '../Login/LoginStrings';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 
@@ -25,16 +26,125 @@ export default class ChatBox extends React.Component{
           this.currentUserDocumentId=localStorage.getItem(LoginString.FirebaseDocumentId);
           this.stateChanged=localStorage.getItem(LoginString.UPLOAD_CHANGED)
           this.currentPeerUser=this.props.currentPeerUser
+          this.groupChatId=null;
+          this.listMessage=[];
+          this.currentPeerUserMessages=[];
+          this.removeListener=null;
+          this.currentPhotoFile=null;
+
+        //   firebase.firestore().collection('users').doc(this.currentPeerUser.documentkey).get()
+        //   .then((docRef)=>{
+        //       this.currentPeerUserMessages=docRef.data().messages
+        //   })
+      }
+      componentDidUpdate(){
+          this.scrollToBottom()
       }
       componentWillReceiveProps(newProps){
           if(newProps.currentPeerUser){
               this.currentPeerUser=newProps.currentPeerUser
-              //this.getListHistory()
+              this.getListHistory()
           }
       }
       componentDidMount(){
-           //this.getListHistory()
+           this.getListHistory()
       }
+      getListHistory=()=>{
+          this.listMessage.length=0
+          this.setState({isLoading:true})
+
+        //   if(
+        //       this.hashString(this.currentUserId) <=
+        //       this.hashString(this.currentPeerUser.id)
+        //   ){
+        //       this.groupChatId=`${this.currentUserId}-${this.currentPeerUser.id}`
+        //   }else{
+        //       this.groupChatId=`${this.currentPeerUser.id}-${this.currentUserId}`
+        //   }
+          
+          //Get history listen new data added
+        //   this.removeListener=firebase.firestore()
+        //   .collection('Messages')
+        //   .doc(this.groupChatId)
+        //   .collection(this.groupChatId)
+        //   .onSnapshot(Snapshot=>{
+        //       Snapshot.docChanges().forEach(change=>{
+        //           if(change.type===LoginString.DOC){
+        //               this.listMessage.push(change.doc.data())
+        //           }
+        //       })
+        //       this.setState({isLoading:false})
+        //   }),
+        //   err=>{
+        //       this.props.showToast(0,err.toString())
+        //   }
+
+      }
+      onSendMessage=(content, type)=>{
+          let notificationMessages=[]
+          if(this.state.isShowStiker && type === 2){
+              this.setState({isShowStiker:false})
+          }
+          if(content.trim() ===''){
+              return
+          }
+          const timestamp=moment()
+          .valueOf()
+          toString()
+
+          const itemMessage={
+              idFrom:this.currentUserId,
+              idTo:this.currentPeerUser.id,
+              timestamp:timestamp,
+              content:content.trim(),
+              type:type
+
+
+          }
+          firebase.firestore()
+          .collection('Messages')
+          .doc(this.groupChatId)
+          .collection(this.groupChatId)
+          .doc(timestamp)
+          .set(itemMessage)
+          .then(()=>{
+              this.setState({inputValue:''})
+          })
+          this.currentPeerUserMessages.map((item)=>{
+              if(item.notificationId !=this.currentUserId){
+                 notificationMessages.push(
+                     {
+                         notificationId:item.notificationId,
+                         number:item.number
+                     }
+                 )
+              }
+          })
+          firebase.firestore()
+          .collection('users')
+          .doc(this.currentPeerUser.documentkey)
+          .update({
+             messages:notificationMessages   
+          })
+          .then((data)=>{})
+          .catch(err=>{
+              this.props.showToast(0,err.toString())
+          })
+
+      }
+      scrollToBottom=()=>{
+          if(this.messagesEnd){
+              this.messagesEnd.scrollIntoView({})
+          }
+      }
+      onKeyboardPress=event=>{
+          if(event.key==='Enter'){
+              this.onSendMessage(this.state.inputValue,0)
+          }
+      }
+     openListSticker=()=>{
+         this.setState({isShowStiker:!this.isShowStiker})
+     } 
     render(){
         return(
          <Card className="viewChatBoard">
@@ -54,7 +164,7 @@ export default class ChatBox extends React.Component{
                   </div>
              </div>
              <div className="viewListContentChat">
-               {/* {} */}
+               {this.renderListMessage()}
                <div
                    style={{float:'left',clear:'both'}}
                    ref={el=>{
@@ -63,7 +173,7 @@ export default class ChatBox extends React.Component{
                />
 
              </div>
-             {this.state.isShowStiker ? this.renderSticker():null}
+             {this.state.isShowStiker ? this.renderStickers():null}
              <div className="viewBottom">
                <img
                    className="icOpenGallery"
@@ -101,10 +211,88 @@ export default class ChatBox extends React.Component{
                />
 
              </div>
+             {this.state.isLoading ? (
+                 <div className="viewLoading">
+                     <ReactLoading
+                         type={'spin'}
+                         color={'#203152'}
+                         height={'3%'}
+                         width={'3%'}
+                     />
+                 </div>
+             ):null}
          </Card>
         )
     }
-    renderSticker=()=>{
+    renderListMessage=()=>{
+        if(this.listMessage.length>0){
+            let viewListMessage=[]
+            this.listMessage.forEach((item,index)=>{
+                if(item.idFrom===this.currentUserId){
+                    if(item.type===0){
+                     viewListMessage.push(
+                         <div className="viewItemRight" key={item.timestamp}>
+                             <span className="textContentItem">{item.content}</span>
+                         </div>
+                     )        
+                    }else if(item.type===1){
+                        viewListMessage.push(
+                            <div className="viewItemLeft2" key={item.timestamp}>
+                                <img
+                                  className="imgItemRight"
+                                  src={item.content}
+                                  alt="Please update your image"
+                                 />
+                            </div>
+                        )
+                    }else{
+                        viewListMessage.push(
+                            <div className="viewItemLeft3" key={item.timestamp}>
+                                <img
+                                    className="imgItemRight"
+                                    src={this.getGifImage(item.content)}
+                                    alt="content message"
+                                />
+                            </div>
+                        )
+                    }
+                }else{
+                   if(item.type===0){
+                       viewListMessage.push(
+                           <div className="viewWrapItemLeft" key={item.timestamp}>
+                               <div className="viewWrapItemLeft3">
+                               {this.isLastMessageLeft(index)?(
+                                   <img
+                                       src={this.currentPeerUser.URL}
+                                       alt="avatar"
+                                       className="peerAvatarLeft"
+                                   />
+                               ):(
+                                   <div className="viewPaddingLeft"/>
+                               )}
+                               <div className="viewItemLeft">
+                                   <span className="textContentItem">{item.content}</span>
+                               </div>
+                               
+
+                               </div>
+                               {this.isLastMessageLeft(index)?(
+                                   <span className="textTimeLeft">
+                                       <div className="time">
+                                           {moment(Number(item.timestamp)).formate('11')}
+                                       </div>
+                                   </span>
+                               ):null}
+                           </div>
+                       )
+                   }else if(item.type===1){
+                       
+                   }
+                }
+            })
+        }
+    }
+    renderStickers=()=>{
         return(
             <div className="viewStickers">
             <img
@@ -130,18 +318,6 @@ export default class ChatBox extends React.Component{
             src={images.lego4}
             alt="sticker"
             onClick={()=>this.onSendMessage('lego4',2)}
-             />
-              <img
-            className="imgSticker"
-            src={images.lego5}
-            alt="sticker"
-            onClick={()=>this.onSendMessage('lego5',2)}
-             />
-              <img
-            className="imgSticker"
-            src={images.lego6}
-            alt="sticker"
-            onClick={()=>this.onSendMessage('lego6',2)}
              />
               <img
             className="imgSticker"
@@ -201,4 +377,35 @@ export default class ChatBox extends React.Component{
             </div>
         )
     }
+    hashString=str=>{
+        let hash=0
+        for(let i=0;i<str.length;i++){
+            hash+=Math.pow(str.charCodeAt(i)*31,str.length - i)
+            hash=hash & hash
+        }
+        return hash
+    }
+    isLastMessageLeft(index){
+        if(
+            (index +1 <this.listMessage.length &&
+                this.listMessage[index +1].idFrom===this.currentUserId) ||
+                index===this.listMessage.length -1
+        ){
+            return true
+        }else{
+            return false
+        }
+    }
+    isLastMessageRight(index){
+        if(
+            (index +1 <this.listMessage.length &&
+                this.listMessage[index +1].idFrom !==this.currentUserId) ||
+                index===this.listMessage.length -1
+        ){
+            return true
+        }else{
+            return false
+        }
+    }
+
 }
